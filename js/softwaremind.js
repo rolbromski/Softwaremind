13515311156798efcbcc413d9e139400488cba9c
rolbromski <rolbromski@gmail.com>
2018-10-31 14:27:25 +0100

    1

--- js/softwaremind.js
+++ js/softwaremind.js
@@ -76,6 +76,52 @@ $(window).ready(function() {
         $('.cookies').fadeOut();
     });
     
+    $('.scrollDown a').click(function(e) {
+        $('html, body').animate({
+            scrollTop: $(".aboutUs").offset().top
+        }, 1000);
+    });
+    
+    $('.scrollTop a').click(function(e) {
+        $('html, body').animate({
+            scrollTop: $(".top").offset().top
+        }, 1000);
+    });
+    
+    //
+    
+    function showMobileMenu(){
+        $(".closeMobile").addClass("active");
+        $(".hamburger").removeClass("active");
+        $(".mobileMenu").addClass("active");
+        $(".mobileMenu ul li").each(function(i) {
+            var el = $(this);
+            setTimeout(function() {
+                el.addClass('active');
+            }, i * 200);
+        });
+    }
+    
+    function hideMobileMenu(){
+        $(".closeMobile").removeClass("active");
+        $(".hamburger").addClass("active");
+        $(".mobileMenu").removeClass("active");
+        $(".mobileMenu ul li").each(function(i) {
+            var el = $(this);
+            setTimeout(function() {
+                el.removeClass('active');
+            }, i * 0);
+        });
+    }
+    
+    $(".hamburger a").click(function(e) {
+        showMobileMenu();
+    });
+    
+    $(".closeMobile a").click(function(e) {
+        hideMobileMenu();
+    });
+    
 });
 
 $(window).on('load', function() {
@@ -87,5 +133,14 @@ $(window).on('resize', function() {
 });
 
 $(window).scroll(function() {
+    
+    var scroll = $(window).scrollTop();
+    
+    if (scroll >= 120) {
+        $(".hamburger").addClass("active");
+    }
+    else {
+        $(".hamburger").removeClass("active");
+    }
     
 });
